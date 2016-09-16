f5527cddcfe109044dca74a96d0ea3c7c5a14e11
Haavard Fagervoll <fagervoll@gmail.com>
2016-09-16 12:58:23 +0200

    Refactoring code.

--- nearby.py
+++ nearby.py
@@ -1,60 +1,47 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import bluetooth
 import time
-import os
-
-os.environ["TZ"] = "Europe/Oslo"
-time.tzset()
-
 
 print("""Paradise: Who's home?""")
 
-havard = 0
-frederik = 0
-raymi = 0
-tormod = 0
+class Person:
+	def __init__(self, name, mac):
+		bluetoothPrescence = False
+		self.name = name
+		self.mac = mac
+	def isPresent():
+		return bluetoothPresence
+	def setPrescence(prescence):
+		self.bluetoothPresence = prescence
+
+
+havard		=	Person("Håvard",		"40:B8:37:2C:C6:9F")
+frederik	=	Person("Frederik",		"F0:24:75:73:CE:7F")
+raymi		=	Person("Raymi",			"84:8E:DF:4B:D7:9F")
+tormod		=	Person("Tormod",		"F4:8E:92:7F:27:12")
+kabbe		=	Person("Jon-Anders",	"F4:8E:92:7F:27:10")
+
+alle = [havard, frederik, raymi, tormod, kabbe]
+
+
+
+def check_home(person):
+	result = bluetooth.lookup_name(person.mac, timeout=5)
+	if (result != None):
+		if not (person.isPresent()):
+			print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()) + "  " + person.name + " har ankommet!")
+			person.setPresence(True)
+
+	elif ( person.isPresent() ) :
+		print( time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()) +"  " + person.name + " har dratt.")
+		person.setPrescence(False)
 
 
 while True:
 
-	result = bluetooth.lookup_name('40:B8:37:2C:C6:9F', timeout=5)
-	if (result != None):
-		if (havard == 0):
-			print(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + "  Haavard har kommet!")
-			havard = 1
-		
-	elif ( havard == 1) :
-		print( time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) +"  Haavard har dratt.")
-		havard = 0
-
-
-	result = bluetooth.lookup_name('F0:24:75:73:CE:7F', timeout=5)
-	if (result != None):
-		if (frederik == 0):
-			print( time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + "  Frederik har kommet!")
-			frederik = 1
-	elif (frederik == 1) :
-		print( time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + "  Frederik har dratt.")
-		frederik = 0
-
-	result = bluetooth.lookup_name('84:8E:DF:4B:D7:9F', timeout=5)
-	if (result != None):
-		if (raymi == 0):
-			print( time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + "  Raymi har kommet!")
-			raymi = 1
-	elif (raymi == 1) :
-		print( time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + "  Raymi har dratt.")
-		raymi = 0
-
-	result = bluetooth.lookup_name('F4:8E:92:7F:27:12', timeout=5)
-	if (result != None):
-		if (tormod == 0):
-			print( time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + "  Tormod har kommet!")
-			tormod = 1
-	elif (tormod == 1) :
-		print( time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + "  Tormod har dratt.")
-		tormod = 0
-
+	for person in alle:
+		check_home(person)
 
 	time.sleep(2)
