213eb6ca008aec57af1a273b38e99b7973156dc9
Server <an@email>
2016-10-20 16:34:43 +0200

    Fix path to ca

--- mqtt_api.py
+++ mqtt_api.py
@@ -4,10 +4,12 @@ data_topic = "paradise/log/presence"
 notify_topic = "paradise/notify/presence"
 everyone_present_topic = "paradise/flamingo/notify"
 
+ca = "/home/pi/who-is-nearby/ca.crt"
+
 def send_presence_event(presence_event):
 	data = str(presence_event)
 
-	publish.single(data_topic, data, port=8883, tls={'ca_certs':"ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+	publish.single(data_topic, data, port=8883, tls={'ca_certs':ca,'tls_version':2}, hostname="nyx.bjornhaug.net")
 	publish.single(notify_topic, data, port=8883, tls={'ca_certs':"ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
 
 def send_everyone_present():
